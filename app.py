ee5248d1bd0fc8f2df27aa7dbf59ad226130fbea
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2023-05-16 11:08:34 +0530

    NLTK

--- app.py
+++ app.py
@@ -1,47 +1,56 @@
 from flask import Flask, request, jsonify
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 
+class TextSummarizer:
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english')) - {'not', 'no'}
+        self.stemmer = SnowballStemmer("english")
+
+    def preprocess_text(self, text):
+        words = [self.stemmer.stem(word) for word in word_tokenize(text.lower()) if word.isalnum() and word not in self.stop_words]
+        return words
+
+    def calculate_word_frequency(self, words):
+        word_freq = {}
+        for word in words:
+            if word not in word_freq:
+                word_freq[word] = 1
+            else:
+                word_freq[word] += 1
+        return word_freq
+
+    def summarize_text(self, text):
+        sentences = sent_tokenize(text)
+        words = self.preprocess_text(text)
+        word_freq = self.calculate_word_frequency(words)
+        sentence_scores = {}
+        for sentence in sentences:
+            for word in word_tokenize(sentence.lower()):
+                if word in word_freq:
+                    if len(sentence.split()) < 50:
+                        if sentence not in sentence_scores:
+                            sentence_scores[sentence] = word_freq[word]
+                        else:
+                            sentence_scores[sentence] += word_freq[word]
+        sorted_sentences = sorted(sentence_scores.items(), key=lambda x: x[1], reverse=True)
+        summary = [sentence.split('.')[0].replace('\n', ' ') for sentence, score in sorted_sentences[:15]]
+        return summary
+
 @app.route('/api/subtitles/<video_id>', methods=['GET'])
 def summarize(video_id):
-    srt = YouTubeTranscriptApi.get_transcript(video_id)
-    paragraph = ' '.join([caption['text'] for caption in srt])
-
-    # Tokenize the paragraph into sentences
-    sentences = sent_tokenize(paragraph)
-
-    # Remove stopwords and stem the words
-    stop_words = set(stopwords.words('english')) - {'not', 'no'}
-    stemmer = PorterStemmer()
-    words = [stemmer.stem(word) for word in word_tokenize(paragraph.lower()) if word.isalnum() and word not in stop_words]
-
-    word_freq = {}
-    for word in words:
-        if word not in word_freq:
-            word_freq[word] = 1
-        else:
-            word_freq[word] += 1
-
-    # Calculate the sentence scores based on word frequency
-    sentence_scores = {}
-    for sentence in sentences:
-        for word in word_tokenize(sentence.lower()):
-            if word in word_freq:
-                if len(sentence.split()) < 50:
-                    if sentence not in sentence_scores:
-                        sentence_scores[sentence] = word_freq[word]
-                    else:
-                        sentence_scores[sentence] += word_freq[word]
-
-    sorted_sentences = sorted(sentence_scores.items(), key=lambda x: x[1], reverse=True)
-
-    summary = [sentence.split('.')[0] for sentence, score in sorted_sentences[:15]]
-
-    return jsonify({'summary_points': summary})
+    try:
+        srt = YouTubeTranscriptApi.get_transcript(video_id)
+        paragraph = ' '.join([caption['text'] for caption in srt])
+        ts = TextSummarizer()
+        summary = ts.summarize_text(paragraph)
+        return jsonify({'summary_points': summary})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(port=5001)
